d77a9d3e70224444e756a234c01d43625cb41f57
Roci Rentama <rocimeli522@gmail.com>
2026-01-30 16:23:53 +0700

    commit pertama

---
+++ app/Views/welcome_message.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Pertama Saya</title>
+</head>
+<body>
+    <h1>Selamat Datang</h1>
+    <p>Ini adalah website dasar saya di VS Code.</p>
+</body>
+</html>
